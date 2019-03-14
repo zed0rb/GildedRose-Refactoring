466583f1e8ae965dc5c1cd498654d48417669f9f
zed0rb <gabrieliusracas@gmail.com>
2019-03-14 11:07:02 +0200

    phpunit set up, fixed gilded_rose_test

--- test/gilded_rose_test.php
+++ test/gilded_rose_test.php
@@ -1,14 +1,14 @@
 <?php
-
+use PHPUnit\Framework\TestCase;
 require_once 'gilded_rose.php';
 
-class GildedRoseTest extends PHPUnit_Framework_TestCase {
+class GildedRoseTest extends TestCase {
 
     function testFoo() {
         $items = array(new Item("foo", 0, 0));
         $gildedRose = new GildedRose($items);
         $gildedRose->update_quality();
-        $this->assertEquals("fixme", $items[0]->name);
+        $this->assertEquals("foo", $items[0]->name);
     }
 
 }
